c7c33122c410914cdf6d195f7641eca1808f9349
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-08 00:10:11 -0300

    feat: add login view

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from '@/presentation/pages/login/login'
+
+const container = document.getElementById('main')
+if (!container) throw new Error('Failed to find the root element')
+const root = createRoot(container)
+root.render(<Login />)
---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as Styles from './spinner-styles.scss'
+
+interface Props extends React.HTMLAttributes<HTMLElement> {}
+
+const Spinner: React.FC<Props> = (props: Props) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+      <div /><div /><div /><div />
+    </div>
+  )
+}
+
+export default Spinner
