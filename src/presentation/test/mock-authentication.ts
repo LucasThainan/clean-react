945931bf7a1671bf27996378bca406987a8eb8f5
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-11 20:19:32 -0300

    test: prevent Login to call submit more than once

--- src/presentation/test/mock-authentication.ts
+++ src/presentation/test/mock-authentication.ts
@@ -5,9 +5,11 @@ import { mockAccountModel } from '@/domain/test'
 export class AuthenticationSpy implements Authentication {
   account = mockAccountModel()
   params!: AuthenticationParams
+  callsCount: number = 0
 
   async auth (params: AuthenticationParams): Promise<AccountModel | undefined> {
     this.params = params
+    this.callsCount++
     return await Promise.resolve(this.account)
   }
 }
