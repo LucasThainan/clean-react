271dfb584c6ff04d546b1def5f9473581bf49c92
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-12 13:56:50 -0300

    feat: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,10 @@
+import { type FieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class EmailValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error | null {
+    return new InvalidFieldError()
+  }
+}
