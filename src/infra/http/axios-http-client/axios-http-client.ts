7de73f48d445d46eb1676c3990706de8a999b39f
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-14 23:30:36 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,14 @@
 import { type HttpResponse, type HttpPostParams, type HttpPostClient } from '@/data/protocols/http'
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (error: any) {
+      httpResponse = error.response
+    }
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,3 +1,3 @@
 export const makeApiUrl = (): string => {
-  return 'http://fordevs.herokuapp.com/api/login'
+  return 'http://localhost:3000/api/login'
 }
