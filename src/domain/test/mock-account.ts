ec6317812c1df19263a27ffe8659e55c92c4f8d3
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-14 23:47:35 -0300

    chore: add files to coverage ignore

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,5 +1,5 @@
 import { type AuthenticationParams } from '@/domain/usecases'
-import { type AccountModel } from '../models'
+import { type AccountModel } from '@/domain/models'
 import { faker } from '@faker-js/faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
