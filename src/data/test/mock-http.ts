63b4371dbca1f94102961a57e64f068fa2f37d59
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-20 21:23:30 -0300

    refactor: rename data mocks

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,10 @@
 import { type HttpPostClient, type HttpPostParams, type HttpResponse, HttpStatusCode } from '@/data/protocols/http'
+import { faker } from '@faker-js/faker'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.internet.userName()
+})
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
