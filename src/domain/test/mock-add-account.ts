1d7ba5f95c56f0ee9a5a131700f552110c551f6b
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-04-20 22:09:24 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,12 @@
+import { type AddAccountParams } from '@/domain/usecases'
+import { faker } from '@faker-js/faker'
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
